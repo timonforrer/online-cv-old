2ce4f6488885296769d4c693779f5dda400f13c2
timonforrer <timon.forrer@gmail.com>
2020-06-28 11:09:34 +0200

    Moved firebase standard config to seperate file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyDEyoW88yU4gCLpjTQxCQm4bcDVZTupp9k",
+  authDomain: "online-cv-57b2e.firebaseapp.com",
+  databaseURL: "https://online-cv-57b2e.firebaseio.com",
+  projectId: "online-cv-57b2e",
+  storageBucket: "online-cv-57b2e.appspot.com",
+  messagingSenderId: "828401225902",
+  appId: "1:828401225902:web:3661d2e8b540b5b6311d3f"
+};
