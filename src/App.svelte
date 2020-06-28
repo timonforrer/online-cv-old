ee624992387b2679ab0241e8dac59d546e6ea413
timonforrer <timon.forrer@gmail.com>
2020-06-28 11:10:59 +0200

    Updated to use login api

--- src/App.svelte
+++ src/App.svelte
@@ -1,68 +1,96 @@
 <script>
+  import { onMount } from 'svelte';
+
+  import queryString from 'query-string';
+
   import { FirebaseApp, User, Doc, Collection } from "sveltefire";
 
   import firebase from "firebase/app";
   import "firebase/firestore";
   import "firebase/auth";
   import "firebase/performance";
-  import "firebase/analytics";
+  // import "firebase/analytics";
 
-  let firebaseConfig = {
-    apiKey: "AIzaSyDEyoW88yU4gCLpjTQxCQm4bcDVZTupp9k",
-    authDomain: "online-cv-57b2e.firebaseapp.com",
-    databaseURL: "https://online-cv-57b2e.firebaseio.com",
-    projectId: "online-cv-57b2e",
-    storageBucket: "online-cv-57b2e.appspot.com",
-    messagingSenderId: "828401225902",
-    appId: "1:828401225902:web:3661d2e8b540b5b6311d3f"
-  };
-
+  import firebaseConfig from '../firebase-config.js';
   firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+
+  let message;
+
+  onMount(async () => {
+    const queryParams = queryString.parse(location.search);
+    const res = await fetch(`/api/login?agency=${queryParams.agency}&token=${queryParams.token}`);
+    const json = await res.json();
+    if (res.ok) {
+      auth.signInWithCustomToken(json.token);
+    }
+    message = json.message;
+  });
 </script>
 
 <main>
+  {#if message}
+    {message}
+  {/if}
+
   <FirebaseApp {firebase}>
 
-  <Collection path={'agencies/ostendis/slides'} let:data={slides}>
+    <User let:user let:auth>
+    
+      <Collection path={`agencies/${user.uid}/slides`} let:data={slides}>
+        {#each slides as slide}
+          {slide.id}
 
-    {#each slides as slide}
-      <section>
-        <h1>{slide.id}</h1>
-        <ul>
-        {#each slide.body as item}
-
-          <li>
-            <h2>{item.type}</h2>
-            {#if item.type === 'columns'}
-              <div class="columns">
-                {#each item.content as column}
-                  
-                  <div class="column">
-                    {#each column.content as column_content}
-                      {#if column_content.type === 'icon'}
-                        icon: {column_content.content}
-                      {:else if column_content.type === 'heading'}
-                        <h3>{column_content.content}</h3>
-                      {:else if column_content.type === 'paragraph'}
-                        <p>{column_content.content}</p>
-                      {/if}
-                    {/each}
-                  </div>
-
-                {/each}
-              </div>
-            {:else if item.content}
-              {item.content}
-            {/if}
-          </li>
-
+          {#each slide.body as item}
+            {item.content}
+          {/each}
         {/each}
-        </ul>
+      </Collection>
+      <div slot="loading">loading</div>
+      <div slot="fallback">fallback</div>
+    </User>
 
-      </section>
-    {/each}
+    <!-- <Collection path={'agencies/ostendis/slides'} let:data={slides}>
 
-  </Collection>
+      {#each slides as slide}
+        <section>
+          <h1>{slide.id}</h1>
+          <ul>
+          {#each slide.body as item}
+
+            <li>
+              <h2>{item.type}</h2>
+              {#if item.type === 'columns'}
+                <div class="columns">
+                  {#each item.content as column}
+                    
+                    <div class="column">
+                      {#each column.content as column_content}
+                        {#if column_content.type === 'icon'}
+                          icon: {column_content.content}
+                        {:else if column_content.type === 'heading'}
+                          <h3>{column_content.content}</h3>
+                        {:else if column_content.type === 'paragraph'}
+                          <p>{column_content.content}</p>
+                        {/if}
+                      {/each}
+                    </div>
+
+                  {/each}
+                </div>
+              {:else if item.content}
+                {item.content}
+              {/if}
+            </li>
+
+          {/each}
+          </ul>
+
+        </section>
+      {/each}
+
+    </Collection> -->
 
   </FirebaseApp>
 
