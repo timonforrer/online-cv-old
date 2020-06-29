f4a3f0e9fff077f171d877df0e21dea5e6c3d811
timonforrer <timon.forrer@gmail.com>
2020-06-29 21:49:12 +0200

    Controller to dynamically load slides

---
+++ src/SlidesController.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let slides;
+  let activeSlide = 1;
+
+  import ComponentsController from './ComponentsController.svelte'
+</script>
+
+<div class="container">
+  {#each slides as slide, i}
+    {#if activeSlide === i}
+      {#each slide.body as item}
+        <ComponentsController activeComponent={item.type} content={item.content} />
+      {/each}
+    {/if}
+  {/each}
+
+  {#if activeSlide > 0}
+    <button on:click={e => activeSlide -= 1}>Previous</button>
+  {/if}
+  {#if activeSlide < (slides.length-1)}
+    <button on:click={e => activeSlide += 1}>Next</button>
+  {/if}
+</div>
