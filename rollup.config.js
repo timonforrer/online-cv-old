49c588c4d17546a206ed1e5b141e747daaaa4275
timonforrer <timon.forrer@gmail.com>
2020-06-30 12:48:44 +0200

    Configured rollup to use svg loader

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import svelteSVG from "rollup-plugin-svelte-svg";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -15,6 +16,7 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		svelteSVG(),
 		svelte({
 			// enable run-time checks when not in production
 			dev: !production,
@@ -24,7 +26,6 @@ export default {
 				css.write('public/build/bundle.css');
 			}
 		}),
-
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
 		// some cases you'll need additional configuration —
