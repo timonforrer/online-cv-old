d96514dfcc0a7aa88b79b665d3e21be2edd1020b
timonforrer <timon.forrer@gmail.com>
2020-06-30 12:48:57 +0200

    Added icons to components

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,5 +1,15 @@
 <script>
-  export let content;
+  export let content = 'coding';
+
+  import Coding from './icons/coding.svg';
+  import Graduate from './icons/graduate.svg';
+  import Work from './icons/work.svg';
+
+  const config = {
+    coding: Coding,
+    graduate: Graduate,
+    work: Work
+  }
 </script>
 
-<pre>ICON {content}</pre>
+<svelte:component this={config[content]} />
