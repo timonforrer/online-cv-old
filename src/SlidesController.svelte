7b7e5e707a4eb968e3070615ac77d5a1e0026799
timonforrer <timon.forrer@gmail.com>
2020-07-02 00:17:05 +0200

    Refined controls and added support for slide layouts

--- src/SlidesController.svelte
+++ src/SlidesController.svelte
@@ -4,12 +4,12 @@
   import ComponentsController from './ComponentsController.svelte';
 
   export let slides;
-  let activeSlide = 1;
+  let activeSlide = 0;
 </script>
 
 <div>
   {#each slides as slide, i}
-    <section class="container slide">
+    <section class="container slide {slide.layout ? slide.layout : ''}">
       {#if activeSlide === i}
         <div transition:fade={{ duration: 300, easing: sineInOut }}>
           {#each slide.body as item}
@@ -22,48 +22,63 @@
 
   <div class="controls">
     <div class="container">
-      <button
-        class="button button--regular" 
-        disabled="{activeSlide <= 0}" 
-        on:click={e => activeSlide -= 1}
-      >
-        Previous
-      </button>
-      <button
-        class="button button--regular" 
-        disabled="{activeSlide >= (slides.length-1)}" 
-        on:click={e => activeSlide += 1}
-      >
-        Next
-      </button>
+      <div class="wrapper">
+        <button
+          class="button button--regular" 
+          disabled="{activeSlide <= 0}" 
+          on:click={e => activeSlide -= 1}
+        >
+          Vorherige Seite
+        </button>
+        <button
+          class="button button--regular" 
+          disabled="{activeSlide >= (slides.length-1)}" 
+          on:click={e => activeSlide += 1}
+        >
+          Nächste Seite
+        </button>
+      </div>
     </div>
   </div>
 </div>
 
 <style>
-.controls {
-  right: 0;
-  bottom: 0;
-  left: 0;
-  position: fixed;
-}
 section.slide {
-  top: 0;
+  top: 3rem;
   right: 0;
   bottom: 0;
   left:  0;
   height: fit-content;
-  margin-bottom: 4rem;
+  margin-bottom: 10rem;
   position: absolute;
 }
+
+section.slide.centered {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .controls {
+  right: 0;
+  bottom: 0;
+  left: 0;
   backdrop-filter: blur(.5rem);
   background-color: rgba(255, 255, 255, 0.6);
+  position: fixed;
 }
-.controls .container {
+
+.controls .wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: -.5rem -.75rem;
   padding-top: 1rem;
   padding-bottom: 1rem;
-  display: flex;
-  justify-content: space-between;
+}
+
+.controls .wrapper > * {
+  margin: .5rem .75rem;
 }
 </style>
