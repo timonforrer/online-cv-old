2aa63a6700b6a5c91148ec850f0855f16e09966a
timonforrer <timon.forrer@gmail.com>
2020-07-03 22:51:27 +0200

    Added technology svgs

---
+++ src/components/Technology.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let technology = '';
+
+  import Airtable from './icons/technologies/airtable.svg';
+  import Firebase from './icons/technologies/firebase.svg';
+  import Gatsby from './icons/technologies/gatsby.svg';
+  import Netlify from './icons/technologies/netlify.svg';
+  import Nextjs from './icons/technologies/nextjs.svg';
+  import React from './icons/technologies/react.svg';
+  import Svelte from './icons/technologies/svelte.svg';
+  import Vercel from './icons/technologies/vercel.svg';
+  import Vuejs from './icons/technologies/vuejs.svg';
+  import Wordpress from './icons/technologies/wordpress.svg';
+
+  const icons = {
+    airtable: Airtable,
+    firebase: Firebase,
+    gatsby: Gatsby,
+    netlify: Netlify,
+    nextjs: Nextjs,
+    react: React,
+    svelte: Svelte,
+    vercel: Vercel,
+    vuejs: Vuejs,
+    wordpress: Wordpress
+  }
+</script>
+
+<svelte:component this={icons[technology]} />
